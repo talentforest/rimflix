9753e3cb7f6ca797d4c4609620802e3d44184263
jellie <talentforest0501@gmail.com>
2022-10-24 00:05:42 +0900

    Apply throttling in useWindowSize hook

--- src/hook/useWindowSize.ts
+++ src/hook/useWindowSize.ts
@@ -3,10 +3,22 @@ import { useEffect, useState } from "react";
 const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState({ width: window.innerWidth });
 
+  const throttle = (func: () => void, delay: number) => {
+    let inProgress = false;
+    return () => {
+      if (inProgress) return;
+      inProgress = true;
+      setTimeout(() => {
+        func();
+        inProgress = false;
+      }, delay);
+    };
+  };
+
   useEffect(() => {
-    function handleResize() {
+    const handleResize = throttle(() => {
       setWindowSize({ width: window.innerWidth });
-    }
+    }, 2000);
 
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
